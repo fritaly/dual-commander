74b31700b3c26f080de39023e2bb7f0ba941ddf2
Francois Ritaly <francois.ritaly@gmail.com>
2014-11-11 04:57:20 -0800

    The class com.github.fritaly.dualcommander.UserPreferences can now fire PropertyChangeEvents

--- src/main/java/com/github/fritaly/dualcommander/UserPreferences.java
+++ src/main/java/com/github/fritaly/dualcommander/UserPreferences.java
@@ -1,6 +1,11 @@
 package com.github.fritaly.dualcommander;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public final class UserPreferences {
+
+	private final PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
 
 	/**
 	 * Whether hidden directories and files should be shown.
@@ -10,11 +15,25 @@ public final class UserPreferences {
 	public UserPreferences() {
 	}
 
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		this.changeSupport.addPropertyChangeListener(listener);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		this.changeSupport.removePropertyChangeListener(listener);
+	}
+
 	public boolean isShowHidden() {
 		return showHidden;
 	}
 
 	public void setShowHidden(boolean showHidden) {
+		final boolean oldValue = this.showHidden;
+
 		this.showHidden = showHidden;
+
+		if (oldValue != showHidden) {
+			this.changeSupport.firePropertyChange("showHidden", oldValue, showHidden);
+		}
 	}
 }
