69d1b9f6bde9e6f002b6f49d165fab338d638b6d
Francois Ritaly <francois.ritaly@gmail.com>
2014-11-11 21:30:56 -0800

    Added method com.github.fritaly.dualcommander.Utils.deleteRecursively(File, FileListener) to recursively delete a directory. Added classes to notify file deletions

--- src/main/java/com/github/fritaly/dualcommander/Utils.java
+++ src/main/java/com/github/fritaly/dualcommander/Utils.java
@@ -17,6 +17,7 @@
 package com.github.fritaly.dualcommander;
 
 import java.awt.Color;
+import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -25,7 +26,42 @@ import java.util.Properties;
 
 import javax.swing.JLabel;
 
+import org.apache.commons.lang.Validate;
+
+import com.github.fritaly.dualcommander.event.FileEvent;
+import com.github.fritaly.dualcommander.event.FileListener;
+
 public class Utils {
+
+	/**
+	 * Recursively deletes the given file or directory and notifies the provided
+	 * listener when a file / directory has been deleted.
+	 *
+	 * @param file
+	 *            the file / directory to delete. Can't be null.
+	 * @param listener
+	 *            a possible listener to be notified of file deletions. Can be
+	 *            null.
+	 */
+	public static void deleteRecursively(File file, FileListener listener) {
+		// The given file can be a file or a directory
+		// The given listener can be null
+		Validate.notNull(file, "The given file is null");
+
+		if (file.isDirectory()) {
+			// Delete the directory recursively
+			for (File entry : file.listFiles()) {
+				deleteRecursively(entry, listener);
+			}
+		}
+
+		// Delete the file or the empty directory
+		file.delete();
+
+		if (listener != null) {
+			listener.fileDeleted(new FileEvent(file));
+		}
+	}
 
 	public static Color getDefaultBackgroundColor() {
 		return new JLabel().getBackground();
