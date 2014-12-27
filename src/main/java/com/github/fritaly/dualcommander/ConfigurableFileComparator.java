589866afeec42b5e2f26baf36532eb03838262bc
Francois Ritaly <francois.ritaly@gmail.com>
2014-12-27 23:51:50 +0100

    Added class com.github.fritaly.dualcommander.ConfigurableFileComparator to configure the comparison of files

---
+++ src/main/java/com/github/fritaly/dualcommander/ConfigurableFileComparator.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.fritaly.dualcommander;
+
+import java.io.File;
+import java.util.Comparator;
+
+import org.apache.commons.lang.Validate;
+
+public final class ConfigurableFileComparator implements Comparator<File> {
+
+	private final HasParentDirectory delegate;
+
+	private SortCriteria criteria = SortCriteria.NAME;
+
+	public ConfigurableFileComparator(HasParentDirectory delegate) {
+		Validate.notNull(delegate, "The given delegate is null");
+
+		this.delegate = delegate;
+	}
+
+	public ConfigurableFileComparator(HasParentDirectory delegate, SortCriteria criteria) {
+		Validate.notNull(delegate, "The given delegate is null");
+		Validate.notNull(criteria, "The given sort criteria is null");
+
+		this.delegate = delegate;
+		this.criteria = criteria;
+	}
+
+	public SortCriteria getCriteria() {
+		return criteria;
+	}
+
+	public void setCriteria(SortCriteria criteria) {
+		Validate.notNull(criteria, "The given sort criteria is null");
+
+		this.criteria = criteria;
+	}
+
+	@Override
+	public int compare(File f1, File f2) {
+		// The parent directory always comes first
+		if (f1.isDirectory() && f1.equals(this.delegate.getParentDirectory())) {
+			return -1;
+		}
+		if (f2.isDirectory() && f2.equals(this.delegate.getParentDirectory())) {
+			return +1;
+		}
+
+		// Compare the 2 files according to the current sort criteria
+		return criteria.compare(f1, f2);
+	}
+}
